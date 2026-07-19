3d5bf364f5e4f7b4d1b3f906284934843567ed20
terrobyte-mg <brayanbacari@gmail.com>
2026-07-19 19:35:51 +0300

    Ajout des donnees pour les noms de ssid pour chaque bande passante pour le hotspot wifi

--- shared/src/commonMain/kotlin/com/terrobytes/cybermanaver2/models/Routeur.kt
+++ shared/src/commonMain/kotlin/com/terrobytes/cybermanaver2/models/Routeur.kt
@@ -3,6 +3,8 @@ package com.terrobytes.cybermanaver2.models
 import com.terrobytes.cybermanaver2.network.NetworkTarget
 
 data class Routeur(
+    val ssid24: String? = null,
+    val ssid5: String? = null,
     val ipAddress: String,
     val macAddress: String? = null,
     val name: String,
