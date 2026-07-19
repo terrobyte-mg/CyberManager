a64405985aeb9e82252fcfccac2d53228b383622
terrobyte-mg <brayanbacari@gmail.com>
2026-07-19 19:44:15 +0300

    Ajout de debug de test de backend sur le backup et les injections de scripts

--- desktopApp/src/main/kotlin/com/terrobytes/cybermanaver2/debug/debugMain3.kt
+++ desktopApp/src/main/kotlin/com/terrobytes/cybermanaver2/debug/debugMain3.kt
@@ -1,23 +1,9 @@
 package com.terrobytes.cybermanaver2.debug
 
-import com.terrobytes.cybermanaver2.network.MikrotikRawClient
-import java.net.Socket
+import java.io.File
 
 fun main() {
 
-    val socket = Socket("192.168.88.1", 8728)
-
-    try {
-        val client = MikrotikRawClient(
-            socket = socket,
-        )
-
-        client.login("admin", "brayan1234567")
-
-        val s = client.execute("/system backup load name=flash/cybermanager-backup.backup password=1234")
-        println(s)
-    } catch (e: Exception) {
-        e.printStackTrace()
-    }
+    println(File(System.getProperty("user.home"), ".cybermanager"))
 
 }
